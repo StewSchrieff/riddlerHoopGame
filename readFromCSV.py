6538b2c36e2ecabcbf0aed3bea398867847d7e5d
Stewart Schrieffer <Stewart.Schrieffer@cyberwoven.com>
2018-08-28 09:15:42 -0400

    fixing bkgd

--- readFromCSV.py
+++ readFromCSV.py
@@ -14,7 +14,7 @@ df_median=  pd.read_csv('df_median.csv')
 sns.set_context("paper")
 sns.set_style("darkgrid")
 sns.regplot(data=df_mean, x=df_mean['hoops'], y=df_mean['steps'], fit_reg=False, color='#008FD5')
-sns.regplot(data=df_median, x=df_median['hoops'], y=df_median['steps'], fit_reg=False, color='#FF8900')
+# sns.regplot(data=df_median, x=df_median['hoops'], y=df_median['steps'], fit_reg=False, color='#FF8900')
 # sns.regplot(x=mean_curve_df['hoops'], y=mean_curve_df['steps'], data=mean_curve_df, scatter_kws={'s':1}, fit_reg=False, color='#008FD5')
 # sns.regplot(x=median_curve_df['hoops'], y=median_curve_df['steps'], data=median_curve_df, scatter_kws={'s':1}, fit_reg=False, color='#f49a41')
 
